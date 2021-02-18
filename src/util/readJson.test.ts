7dcedf4732d7f8217ec4f8b06bf4ec855d35f505
Dan <code@haddigan.email>
2021-02-18 08:39:17 -0500

    add pagination utilities

--- src/util/readJson.test.ts
+++ src/util/readJson.test.ts
@@ -1,16 +1,32 @@
-import { getAllUsers } from "./readJson";
+import { getAllUsers, getPaginatedUsers } from "./readJson";
 const users = require("../../data/users.json");
-const trimmedUsers = users.splice(0, 4);
-
-jest.mock("fs-extra", () => ({
-  readJson: () => trimmedUsers,
-}));
+const trimmedUsers = users.slice(0, 4);
+const usersLength = users.length;
 
 describe("readJson module", () => {
-  it("should get the entire list of users from json", async () => {
+  it("getAllUsers should get the entire list of users from json", async () => {
     const result = await getAllUsers();
+    const trimmedResult = result.slice(0, 4);
+    expect(result).toHaveLength(usersLength);
+    expect(JSON.stringify(trimmedResult)).toMatch(JSON.stringify(trimmedUsers));
+  });
 
-    expect(result).toHaveLength(4);
-    expect(JSON.stringify(result)).toMatch(JSON.stringify(trimmedUsers));
+  describe("getPaginatedUsers", () => {
+    it.each([
+      [5, 3],
+      [2, 10],
+      [5, 5],
+    ])("should get paginated user data", async (PAGE, LENGTH) => {
+      const result = await getPaginatedUsers(PAGE, LENGTH);
+      const firstResult = users[(PAGE - 1) * LENGTH];
+      expect(result[0]).toMatchObject(firstResult);
+      expect(result).toHaveLength(LENGTH);
+    });
+
+    it("should properly handle out of bounds requests", async () => {
+      const outOfBoundsResult = await getPaginatedUsers(100, 5);
+
+      expect(outOfBoundsResult).toHaveLength(0);
+    });
   });
 });
--- src/util/readJson.ts
+++ src/util/readJson.ts
@@ -6,3 +6,14 @@ export async function getAllUsers(): Promise<User[]> {
   const users = readJson(USER_FILE_PATH);
   return users;
 }
+
+export async function getPaginatedUsers(page: number, limit: number) {
+  const pageIndex = page - 1;
+  const users = await readJson(USER_FILE_PATH);
+  const initialIndex = pageIndex * limit;
+  const finalIndex = initialIndex + limit;
+
+  if (initialIndex > users.length) return [];
+
+  return users.slice(initialIndex, finalIndex);
+}
